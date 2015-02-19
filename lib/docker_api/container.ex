378a977c783aabd35a174a6008f809406b9b41db
Bradley Smith <bradleydsmith@gmail.com>
2015-02-18 17:35:55 -0700

    still working out basic request response flow

--- lib/docker_api/container.ex
+++ lib/docker_api/container.ex
@@ -1,58 +1,54 @@
 defmodule DockerApi.Container do
 
   def all(host) when is_binary(host) do
-    response = HTTPoison.get host <> "/containers/json" 
+    response = HTTPoison.get host <> "/containers/json?all=1" 
+    handle_response(response)
+  end
+
+  def all(host, opts) when is_map(opts) do
+    response = HTTPoison.get host <> "/containers/json?#{query_params(opts)}" 
     handle_response(response)
   end
 
   def get(host, id) do
-    case HTTPoison.get host <> "/containers/#{id}/json" do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, %{body: body, status_code: 200}}
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        %{body: "Not found", status_code: 404} 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        %{body: reason, status_code: 500}
-    end 
-    |> 
-    parse_response 
+    response =HTTPoison.get host <> "/containers/#{id}/json"
+    handle_response(response)
   end
   
  
   def top(host, id) do
-    response = HTTPoison.get host <> "/containers/#{id}/top/json"
-    response |>
-    parse_response 
+    response = HTTPoison.get host <> "/containers/#{id}/top"
+    handle_response(response)
   end
   
   def changes(host, id) do
-    response = HTTPoison.get host <> "/containers/#{id}/changes/json"
-    response |>
-    parse_response 
+    response = HTTPoison.get host <> "/containers/#{id}/changes"
+    handle_response(response)
   end
   
-  def start(host, id, opts \\ %{}) do
-    {:ok, response} = HTTPoison.post host <> "/containers/#{id}/start", opts
-    response |>
-    parse_response
+  def start(host, id) do
+    response = HTTPoison.post host <> "/containers/#{id}/start", ""
+    handle_response(response)
+  end
+
+  def start(host, id, opts) do
+    response = HTTPoison.post host <> "/containers/#{id}/start", Poison.encode!(opts),  %{"Content-type" => "application/json"}
+    handle_response(response)
   end
 
   def stop(host, id) do
-    {:ok, response } = HTTPoison.post host <> "/containers/#{id}/stop"
-    response |>
-    parse_response
+    response = HTTPoison.post host <> "/containers/#{id}/stop", ""
+    handle_response(response)
   end
 
   def restart(host, id) do
     response = HTTPoison.post host <> "/containers/#{id}/restart"
-    response |>
-    parse_response
+    handle_response(response)
   end
 
   def kill(host, id) do
     response = HTTPoison.post host <> "/containers/#{id}/kill"
-    response |>
-    parse_response
+    handle_response(response)
   end
 
   def logs(host, id) do
@@ -85,6 +81,14 @@ defmodule DockerApi.Container do
   def handle_response(resp = {:ok, %{status_code: 200, body: body}}) do
     parse_response(resp)
   end
+ 
+  def handle_response(resp = {:ok, %{status_code: 204, body: body}}) do
+    parse_response(resp)
+  end
+ 
+  def handle_response(resp = {:ok, %{status_code: 304, body: body}}) do
+    parse_response(resp)
+  end
   
   def handle_response(resp = {:ok, %{status_code: 404 , body: body}}) do 
     parse_response(resp)
@@ -97,11 +101,26 @@ defmodule DockerApi.Container do
   def parse_response({:error, resp = %HTTPoison.Error{id: _, reason: reason}}) do
     { :error, reason, 500 }
   end
+  
+  def parse_response({:ok, resp=%HTTPoison.Response{body: "", headers: headers, status_code: code}}) do
+    {:ok, resp.body, code }
+  end
+
+  def parse_response({:ok, resp=%HTTPoison.Response{body: body, headers: %{"Content-Length" =>  _, "Content-Type" => "text/plain; charset=utf-8", "Date" => _}, status_code: code}}) do
+    {:ok, body, code }
+  end
 
   def parse_response(response) do
     {result, response } = response 
-    { result, Poison.decode!(response.body), response.status_code }
+    {result, Poison.decode!(response.body), response.status_code }
   end
 
+  def query_params(args)  do
+   Enum.map(Map.to_list(args), fn {k,v} -> encode_attribute(k, v) end)
+   |> Enum.join("&") 
+  end
   
+  def encode_attribute(k, v), do: "#{k}=#{encode_value(v)}"
+
+  def encode_value(v), do: URI.encode_www_form("#{v}")
 end
--- test/containers_test.exs
+++ test/containers_test.exs
@@ -2,36 +2,44 @@ defmodule DockerApiContainerTest do
   use ExUnit.Case
 
   @host "192.168.4.4:14443"
+  @cid "971f52624eb3"
 
   test "/containers" do
-    body = DockerApi.Container.all("192.168.4.4:14445")
-    IO.inspect body
-    #assert is_list(body) 
+    {:ok, body, code }  = DockerApi.Container.all(@host)
+    assert is_list(body) 
+  end
+
+  test "/containers with options" do
+    {:ok, body, code }  = DockerApi.Container.all(@host, %{all: 1, limit: 1, size: 1})
+    assert is_list(body) 
   end
 
   test "/containers/id" do
-    {:ok, body, code }  = DockerApi.Container.get("192.168.4.4:14443", "971f52624eb3")
+    {:ok, body, code }  = DockerApi.Container.get(@host, @cid)
     assert is_map(body) 
   end
  
   test "/containers/id/top" do
-    { body, code } = DockerApi.Container.top("192.168.4.4:14443", "d869ff642538359d")
-    IO.inspect code
+    { :ok, body, code } = DockerApi.Container.top(@host, @cid)
+    assert is_map(body) 
   end
 
   test "/containers/id/changes" do
-    { body, code } = DockerApi.Container.changes("192.168.4.4:14443", "d869ff642538359d")
-    IO.inspect code
+    { :ok, body, code } = DockerApi.Container.changes(@host, @cid)
+    assert is_map(body) 
   end
   
-  test "/containers/id/start" do
-    { body, code } = DockerApi.Container.start("192.168.4.4:14443", "971f52624eb325de17737283b5096546ec59db8e541b70d5bfa0cd8846d6560b")
-    IO.inspect code
+  test "/containers/id/start with port options" do
+    payload = %{  "PortBindings": %{ "22/tcp": [%{ "HostIp": "192.168.4.4" }], "6379/tcp": [%{ "HostIp": "192.168.4.4" }]}}
+    {:ok, body, code } = DockerApi.Container.start(@host, @cid, payload)
+  end
+
+  test "/containers/id/start with no options" do
+    {:ok, body, code } = DockerApi.Container.start(@host, @cid)
   end
 
   test "/containers/id/stop" do
-    { body, code } = DockerApi.Container.start("192.168.4.4:14443", "d869ff642538359d")
-    IO.inspect code
+    { :ok, body, code } = DockerApi.Container.stop(@host, @cid)
   end
 
 end
