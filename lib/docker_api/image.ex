baeab95336fcf6d68ecf8d2a77f3223a91397d33
Bradley Smith <bradleydsmith@gmail.com>
2015-02-19 20:36:26 -0700

    remove cruft

--- lib/docker_api/image.ex
+++ lib/docker_api/image.ex
@@ -17,40 +17,4 @@ defmodule DockerApi.Image do
     handle_response(response)
   end
 
-  #def search(host, search_term) when is_binary(host) and is_binary(search_term) do
-    #parse_get_response(host <> "/images/search?term=#{search_term}")
-  #end
-
-  #defp parse_get_response(url) do
-    #case HTTPoison.get(url) do
-      #{:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        #{ decode_json(body), 200 }
-      #{:ok, %HTTPoison.Response{status_code: 404}} ->
-        #IO.puts "Not found :("
-        #{ [], 404 }
-      #{:error, %HTTPoison.Error{reason: reason}} ->
-        #IO.inspect reason
-        #{ reason, 500 }
-    #end
-
-  #end
-
-  #defp parse_response(:post, url) do
-    #case HTTPoison.post(url) do
-      #{:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        #{ decode_json(body), 200 }
-      #{:ok, %HTTPoison.Response{status_code: 404}} ->
-        #IO.puts "Not found :("
-        #{ [], 404 }
-      #{:error, %HTTPoison.Error{reason: reason}} ->
-        #IO.inspect reason
-        #{ reason, 500 }
-    #end
-
-  #end
-
-  #defp decode_json(body) do
-    #{:ok, body} = Poison.decode(body)
-    #body
-  #end
 end
