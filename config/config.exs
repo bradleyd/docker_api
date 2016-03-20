fc8c2def6e7174cc7db91365324c23411d4bcbdf
nemski <therealpatrobinson@gmail.com>
2016-03-20 23:48:33 +1100

    Configure a more sane default for tests

--- config/config.exs
+++ config/config.exs
@@ -22,5 +22,5 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-config :docker_api, :hosts,
-  ["docker0001.wameku.com:14443", "192.168.4.4:14443"]
+
+import_config "#{Mix.env}.exs"
---
+++ config/test.exs
@@ -0,0 +1,2 @@
+use Mix.Config
+config :docker_api, :host, "127.0.0.1:2376"
